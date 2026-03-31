51a5a0dd0e164042fc9bb043abbeceedb8327ad3
keysha-adesi <keyshaputrii590@gmail.com>
2026-03-31 21:52:07 +0700

    perulangan dengan DO

--- loop/looping2.cpp
+++ loop/looping2.cpp
@@ -1,56 +1,94 @@
 #include <iostream>
-#include <string>
+#include <ctime>
 using namespace std;
 
-// Deklarasi variable global
-int kodeJurusan;
-string namaJurusan;
-string statusKuota;
-
-// prosedur input
-void input() {
-    cout << "=== PENDAFTARAN JURUSAN KAMPUS ===" << endl;
-    cout << "1. Teknologi Informasi" << endl;
-    cout << "2. Teknik Elektro" << endl;
-    cout << "3. Sistem Informasi" << endl;
-    cout << "----------------------------------" << endl;
-    cout << "Masukkan kode jurusan (1-3): ";
-    cin >> kodeJurusan;
-}
-
-// prosedur untuk pengecekan kondisi
-void pilihanJurusan() {
-    switch (kodeJurusan) {
-        case 1:
-            namaJurusan = "Teknologi Informasi";
-            statusKuota = "Tersedia (15 Kursi)";
-            break;
-        case 2:
-            namaJurusan = "Teknik Elektro";
-            statusKuota = "Kuota Penuh!";
-            break;
-        case 3:
-            namaJurusan = "Sistem Informasi";
-            statusKuota = "Tersedia (5 Kursi)";
-            break;
-        default:
-            namaJurusan = "Tidak Diketahui";
-            statusKuota = "Error: Kode Tidak Valid";
-    }
-}
-
-// prosedur output
-void output() {
-    cout << "\n=== HASIL SELEKSI ===" << endl;
-    cout << "Jurusan Pilihan : " << namaJurusan << endl;
-    cout << "Status Kuota    : " << statusKuota << endl;
-    cout << "----------------------------------" << endl;
-}
-
 int main() {
-    input();
-    pilihanJurusan();
-    output();
+    int x;
+
+    //Perulangan dengan FOR
+    //mencetak tulisan seabnyak 5 kali
+    cout<<"PERULANGAN FOR"<<endl;
+    for(int i=0;i<=4;i++){
+        cout<<"Teknologi Informasi UMY"<<endl;   
+    }
+    cout<<endl;
+
+    //untuk mendapatkan urutan angka acak yg berbeda
+    srand(time(0));
+
+    //Perulangan dengan WHILE
+    cout<<"PERULANGANN WHILE"<<endl;
+    x = 1 + rand()%10;
+
+    while(x<=5){
+        cout<<"Bilangan acak = "<<x <<endl;
+        x = rand()%10;
+    }
+    cout << "Bilangan acak while yang tearkhir =" << x <<endl;
+    cout <<endl;
+
+    //Perulangan dengan DO .. .WHILE
+    cout <<"PERULANGANN DO ... WHILE"<<endl;
+    x = 1 + rand()%10;
+
+    do{
+        cout << "Bilangan acak = "<<x << endl;
+        x = rand()%10;
+    }while(x <= 5);
+
+    cout<<"Bilangan acak do-while yang terakhir ="<<x << endl;
+    cout<<endl;
+
+
+    // Logika not
+    float nilB, nilM, rerata;
+    string status;
+
+    cout<<"Masukkan nilai Matematika = ";
+    cin>>nilM;
+    cout<<"Masukkan nilai Bahasa Inggris = ";
+    cin>>nilB;
+
+    rerata = (nilB+nilM)/2;
+
+    if (!(rerata < 60))
+        status = "Lulus";
+    else
+        status = "Tidak lulus";
+
+    cout<<"Status kelulusan = "<<status<< " ,dengan nilai rata-rata = "<<rerata<<endl;
+
+
+    // Logika OR
+    cout<<"\nMasukkan nilai Matematika = ";
+    cin>>nilM;
+    cout<<"Masukkan nilai Bahasa Inggris = ";
+    cin>>nilB;
+
+    rerata = (nilB+nilM)/2;
+
+    if ((rerata >= 60) || (nilM >=70))
+        status = "Lulus";
+    else
+        status = "Tidak lulus";
+
+    cout<<"Status kelulusan = "<<status<<" ,dengan nilai rata-rata = " << rerata << endl;
+
+
+    // Logika AND
+    cout<<"\nMasukkan nilai Matematika = ";
+    cin>>nilM;
+    cout<<"Masukkan nilai Bahasa Inggris = ";
+    cin>>nilB;
+
+    rerata = (nilB+nilM)/2;
+
+    if ((rerata >= 60) && (nilM >= 70))
+        status = "Lulus";
+    else
+        status = "Tidak lulus";
+
+    cout << "Status kelulusan = " << status << " ,dengan nilai rata-rata = " << rerata << endl;
 
     return 0;
 }
